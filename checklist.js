cc0c4285b723f0f1928401e01a93db73ab4e9bc0
NobreDev11 <nobre.dev11@gmail.com>
2025-05-09 18:04:29 -0300

    Adiciona cliente.html e integra redirecionamento

--- checklist.js
+++ checklist.js
@@ -1,2 +1,6 @@
 console.log('Check List de Recebimento carregado');
 // Lógica de envio ou navegação pode ser adicionada depois
+document.getElementById('btn-novo-cliente').addEventListener('click', () => {
+    window.location.href = 'cliente.html';
+  });
+  
